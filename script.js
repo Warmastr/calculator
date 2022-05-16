8324800edda7afe31e99bf11ee5107032be2ef0a
warmastr <jargyle80@yahoo.com>
2022-05-16 13:47:06 -0500

    added the calculator display and created container for calc buttons

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
+/* When using a calculator, the user inputs a number, then selects an operator, then another number and then presses the equals button to return a result. My job will be to capture the numbers the user entered and store the result as a variable. If an operator is pressed, whatever the user entered before that is the first number, then when the equals sign is pressed, whatever numbers the user entered prior to that is the second number*/
+
+let result = 0;
+
 const add = function(intA, intB) {
-	let result = 0;
     result = intA + intB;
     return result;
 };
 
 const subtract = function(intA, intB) {
-	let result = 0;
     result = intA - intB;
     return result;
 };
 
-const multiply = function(arr) {
-    let result = 1;
-    for (let i=0; i < arr.length; i++)
-      result = result *= arr[i];
+const multiply = function(num1, num2) {
+    result = num1 * num2;
     return result;
 };
 
 const divide = function(dividend, divisor) {
-    let result = 0;
     result = dividend / divisor;
     if (divisor == 0) {
         alert("Let's say you have 10 cookies, what then is the number of cookies each person would receive if those 10 cookies were divided amongst 0 people? I'll wait...")
@@ -27,7 +26,7 @@ const divide = function(dividend, divisor) {
 };
 
 const power = function(num, pwr) {
-    let result = 1;
+    result = 1;
 	// num needs to be multiplied by itself pwr times
     for (let i = 0; i < pwr; i++) {
       result = result * num;
@@ -36,7 +35,7 @@ const power = function(num, pwr) {
 };
 
 const factorial = function(n) {
-	let result = n;
+	result = n;
     if (n === 0 || n === 1) {
         result = 1;
     } else if (n > 1) {
@@ -50,4 +49,8 @@ const factorial = function(n) {
     return result;
 };
 
-console.log(divide());
+const operate = function(operator, num1, num2) {
+    
+}
+
+console.log(multiply(6,3));
