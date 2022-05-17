141481ccd556d25d4619cf71779daf9650cc131e
warmastr <jargyle80@yahoo.com>
2022-05-17 13:47:30 -0500

    buttons and styling implemented, renamed stylesheet.css to style.css

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 /* When using a calculator, the user inputs a number, then selects an operator, then another number and then presses the equals button to return a result. My job will be to capture the numbers the user entered and store the result as a variable. If an operator is pressed, whatever the user entered before that is the first number, then when the equals sign is pressed, whatever numbers the user entered prior to that is the second number*/
 
 let result = 0;
-const buttonContainer = document.querySelector('.buttonContainer');
-
-const repeatString = function(string, num) {
-    const err = 'ERROR';
-    let result = '';
-    if (num < 0) result = err;
-    for (let i = 0; i < num; i++) {
-        result += string;
-        }
-    return result
-};
-
-let col = repeatString('1fr ', 4);
-let row = repeatString('1fr ', 5);
-let oldName = 'button';
-let newName = oldName + 1;
-    // create columns
-    for (let i = 1; i <= 4; i++) {
-        // create rows
-        for (let j = 1; j <=5; j++) {
-            let buttons = document.createElement('div');
-            buttonContainer.append(buttons);
-            buttons.className = oldName;
-            if (buttons.className === oldName) {
-                buttons.className = newName;
-            }
-            buttonContainer.style.gridTemplateColumns = `${col}`;
-            buttonContainer.style.gridTemplateRows = `${row}`;
-        }
-    }
 
 const add = function(intA, intB) {
     result = intA + intB;
