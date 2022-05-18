d13e994f6060e0057cda516a6194712da49616d6
warmastr <jargyle80@yahoo.com>
2022-05-17 22:29:39 -0500

    WIP addition operator

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /* When using a calculator, the user inputs a number, then selects an operator, then another number and then presses the equals button to return a result. My job will be to capture the numbers the user entered and store the result as a variable. If an operator is pressed, whatever the user entered before that is the first number, then when the equals sign is pressed, whatever numbers the user entered prior to that is the second number*/
 
 let result = 0;
-let operator;
+let nums = [];
+let operator = [];
 const display = document.querySelector('.display');
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
@@ -14,48 +15,60 @@ const eight = document.querySelector('.eight');
 const nine = document.querySelector('.nine');
 const zero = document.querySelector('.zero');
 const clear = document.querySelector('.clear');
+const square = document.querySelector('.square');
+const percent = document.querySelector('.percent');
+const divided = document.querySelector('.divide');
+const times = document.querySelector('.multiply');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const fact = document.querySelector('.factorial');
+const point = document.querySelector('.point');
+const equals = document.querySelector('.equals');
+
+plus.addEventListener('click', () => {
+});
 
 clear.addEventListener('click', () => {
     display.textContent = '';
 });
 one.addEventListener('click', () => {
-    display.textContent = display.textContent + '1';
+    display.textContent += '1';
 });
 
 two.addEventListener('click', () => {
-    display.textContent = display.textContent + '2';
+    display.textContent += '2';
 });
 
 three.addEventListener('click', () => {
-    display.textContent = display.textContent + '3';
+    display.textContent += '3';
 });
 
 four.addEventListener('click', () => {
-    display.textContent = display.textContent + '4';
+    display.textContent += '4';
 });
 
 five.addEventListener('click', () => {
-    display.textContent = display.textContent + '5';
+    display.textContent += '5';
 });
 
 six.addEventListener('click', () => {
-    display.textContent = display.textContent + '6';
+    display.textContent += '6';
 });
 
 seven.addEventListener('click', () => {
-    display.textContent = display.textContent + '7';
+    display.textContent += '7';
 });
 
 eight.addEventListener('click', () => {
-    display.textContent = display.textContent + '8';
+    display.textContent += '8';
 });
 
 nine.addEventListener('click', () => {
-    display.textContent = display.textContent + '9';
+    display.textContent += '9';
 });
 
 zero.addEventListener('click', () => {
-    display.textContent = display.textContent + '0';
+    display.textContent += '0';
 });
 
 const add = function(num1, num2) {
@@ -105,9 +118,28 @@ const factorial = function(n) {
     return result;
 };
 
+const percentage = function(num1) {
+    return num1 / 100;
+}
+
+plus.addEventListener ('click', () => {
+    nums.push(parseInt(display.textContent));
+    display.textContent += '+';
+    nums.push('+');
+    console.log(nums[0]);
+    console.log(nums[1]);
+    operator.push(add());
+    console.log(operator[0]);
+});
+
 const operate = function(operator, num1, num2) {
     
 }
 
+equals.addEventListener('click', () => {
+    
+    display.textContent += nums[0,1];
+});
 
+/* user enters first number to add, user presses the "plus" sign, number is stored to 'num1', display is cleared and '+' sign is highlighted, and the user then enters the next number to add, then presses 'equals'. When the user presses equals, the number the user entered is stored to 'num2' and the add() function is called with num1 and num2 as parameters. */
 
